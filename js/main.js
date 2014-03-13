75a69f11b762c8e46347d2817c0d2ca5b09da645
Bill Wood <bill@bluesquare.comlu.com>
2014-03-13 09:52:56 -0400

    After some silly fights with the public key I finally have the connection configured to git from Aptana.
    I have put all of my cdn library connections in place and verified that jquery is loading properly

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 /**
  * @author Wood
+ * This should hols all of my additional functionality. Probably for initializing the collection object.
  */
 
 jQuery.noConflict();
 (function(bw){				
 	//JQUERY
 	bw(function() {
-		alert(mwahahaha);
+		alert('mwahahaha');
 	});
 })(jQuery);
